ec05d0a26142ccb859e38256aca0ba74d82df1b5
GuerinTifenn <8807@holbertonstudents.com>
2024-03-25 14:23:08 +0000

    update main.h

--- main.h
+++ main.h
@@ -1,26 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main.h
- * Auth: Tifenn GUERIN and Nicolas BRAULT DOMINGO
- * Desc: Header file contaning the structure and protoypes used in printf.c
- */
+#include <stdarg.h>
 
-/** 
- * struct format
+/**
+ * struct format - Format specifier and join print function
  * @tools: The type of the character
  * @print: The associeted function
  */
-    typedef struct format
-    {
-    char *tools;
-    int (*print)(va_list);
-    } format_t;
 
-    int _printf(const char* format, ...);
-    int _putchar(char c);
-	int print_char(va_list args);
-	int print_string(va_list args);
-	int print_percent(va_list args);
+typedef struct format
+{
+	char *tools;
+	int (*print)(va_list);
+} format_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
 
 #endif
