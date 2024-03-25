fc3a19b11b8dd30728aa5e7051ace46957e0ace7
GuerinTifenn <8807@holbertonstudents.com>
2024-03-25 12:05:20 +0000

    add fonction.h and add prototype in main.h

--- main.h
+++ main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
-* File: main.h
-* Auth: Tifenn GUERIN and Nicolas BRAULT DOMINGO
-* Desc: Header file contaning the structure and protoypes used in printf.c
-/
- /** 
+/**
+ * File: main.h
+ * Auth: Tifenn GUERIN and Nicolas BRAULT DOMINGO
+ * Desc: Header file contaning the structure and protoypes used in printf.c
+ */
+
+/** 
  * struct format
  * @tools: The type of the character
  * @print: The associeted function
@@ -18,4 +19,8 @@
 
     int _printf(const char* format, ...);
     int _putchar(char c);
+	int print_char(va_list args);
+	int print_string(va_list args);
+	int print_percent(va_list args);
+
 #endif
