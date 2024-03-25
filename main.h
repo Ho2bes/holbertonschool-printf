b02ca4c092c5aa9620932cc3597813bca566de32
Ho2bes <8696@holbertonstudents.com>
2024-03-25 11:59:15 +0000

    push main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+* File: main.h
+* Auth: Tifenn GUERIN and Nicolas BRAULT DOMINGO
+* Desc: Header file contaning the structure and protoypes used in printf.c
+/
+ /** 
+ * struct format
+ * @tools: The type of the character
+ * @print: The associeted function
+ */
+    typedef struct format
+    {
+    char *tools;
+    int (*print)(va_list);
+    } format_t;
+
+    int _printf(const char* format, ...);
+    int _putchar(char c);
+#endif
